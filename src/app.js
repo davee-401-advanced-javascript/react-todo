102e25e9f7a180ff053fcac4c639f6f36c5a3152
Davee Sok <daveesok@gmail.com>
2020-11-18 18:04:04 -0800

    initial wiring of header

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
-
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Navbar';
-import FormControl from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+// import NavDropdown from 'react-bootstrap/Navbar';
+// import Form from 'react-bootstrap/Form';
+// import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <>
-        <Navbar bg="dark" variant="dark">
-          <span>John</span>
-        </Navbar>
-        <div>Hello World!</div> 
-  </>
+      <Navbar bg="primary" variant="dark">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav>
+        <Button variant="danger">Log Out</Button>
+      </Navbar>
+
+    </>
   );
 }
 
