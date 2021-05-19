9cb3e0bf16a48848c33d73b9643607bf83bc33ce
Davee Sok <daveesok@gmail.com>
2021-05-18 23:54:51 -0500

    cleaning up

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,22 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
 
 import LogIn from '../../context/auth/login.js';
-// import {SettingsContext} from '../../context/settings-context.js';
 
 function Header(props) {
-  // const settingsContext = useContext(SettingsContext);
-
   return (
     <Navbar bg="primary" variant="dark">
       <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
       </Nav>
       <LogIn />
-      {/* <Button variant="danger">Log Out</Button> */}
-      {/* <Button variant="success" onClick={globalContext.toggleMode}>{globalContext.mode}</Button> */}
     </Navbar>
   );
 }
