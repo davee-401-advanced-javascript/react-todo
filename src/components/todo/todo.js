f27f69e56e34a39c52de58518d207fbf4a3bdd61
Davee Sok <daveesok@gmail.com>
2021-05-19 21:24:46 -0500

    attempting to deploy

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
